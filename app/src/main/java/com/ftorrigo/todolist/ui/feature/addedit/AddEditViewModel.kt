8698a3b07fb9084145dda2a036d9ebb2fda865e4
Fernando Torrigo <torrigofernando@gmail.com>
2024-12-21 14:09:51 +0000

    Implementing viewModel

---
+++ app/src/main/java/com/ftorrigo/todolist/ui/feature/addedit/AddEditViewModel.kt
@@ -0,0 +1,53 @@
+package com.ftorrigo.todolist.ui.feature.addedit
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ftorrigo.todolist.data.TodoRepository
+import com.ftorrigo.todolist.ui.UiEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+class AddEditViewModel(
+    private val repository: TodoRepository
+) : ViewModel() {
+
+    var title by mutableStateOf("")
+        private set
+
+    var description by mutableStateOf("")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event: AddEditEvent) {
+        when (event) {
+            is AddEditEvent.TitleChanged -> {
+                title = event.title
+            }
+
+            is AddEditEvent.DescriptionChanged -> {
+                description = event.description
+            }
+
+            AddEditEvent.Save -> {
+                saveTodo()
+            }
+        }
+    }
+
+    private fun saveTodo() {
+        viewModelScope.launch {
+            if (title.isBlank()) {
+                _uiEvent.send(UiEvent.ShowSnackbar("Title cannot be empty"))
+                return@launch
+            }
+            repository.insert(title = title, description = description)
+            _uiEvent.send(UiEvent.NavigateBack)
+        }
+    }
+}
